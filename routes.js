bea82fd5e5ab85bf24be625aacd64c6b63882bb2
Peter <peter.lim91@gmail.com>
2021-01-15 12:58:57 +1300

    templates updated with working references

--- routes.js
+++ routes.js
@@ -11,10 +11,15 @@ router.use(express.urlencoded({
   extended: false
 }))
 
+router.post('/', (req, res) => {
+  console.log(req.body)
+})
+
 
 router.get('/', (req, res) => {
-  const viewData = utils.exampleData 
-  res.render('home')
+  const viewData = utils.exampleData()
+  console.log(viewData)
+  res.render('home', viewData)
 })
 
 router.post('/add', (req, res) => {
