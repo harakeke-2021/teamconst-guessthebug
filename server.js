86b4fbc7ab332af302412f70ac29c523d52c1c03
Mariano Zeppa <>
2021-01-15 10:40:51 +1300

    post /moth/:id

--- server.js
+++ server.js
@@ -7,4 +7,17 @@ const hbs = require('express-handlebars')
 
 // './' get route
 // './' post route for handling the guess, and redirecting to correct/wrong answer page.
+
+server.post('/moth/:id/', function (req, res) {
+    const theMoth = req.body
+    if (bugs.bugs.id = theMoth.id){
+        const winorlose = win
+    } else 
+    const winorlose = lose
+    res.send(`/${winorlose}`)
+}
+
+
+
+
 // './:id' for just displaying the moths not required for MVP but should be easy enough to implement very quickly.
