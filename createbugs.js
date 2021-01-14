071e4c7d24697f6522f327aeea3e7e9a2c948757
steve-kim-96 <73867233+steve-kim-96@users.noreply.github.com>
2021-01-14 18:30:08 +1300

    update up to bug 6

--- createbugs.js
+++ createbugs.js
@@ -1,31 +1,52 @@
 const bugs = {}
 bugs.bugs = []
+
 const bug1 = {}
 bug1.name = 'Green pergesa hawkmoth'
 bug1.image = './public/images/moth1.jpeg'
 bug1.hint = 'placeholder hint'
 bug1.id = 1
 bug1.fact = 'placeholder fact'
-
 bugs.bugs.push(bug1)
+
 const bug2 = {}
 bug2.name = 'Luna Moth'
+bug2.image = './public/images/moth2.jpeg'
+bug2.hint = 'placeholder hint'
+bug2.id = 2
+bug2.fact = 'placeholder fact'
 bugs.bugs.push(bug2)
 
 const bug3 = {}
-bug3.name = 'Comet moth '
+bug3.name = 'Comet moth'
+bug3.image = './public/images/moth3.jpeg'
+bug3.hint = 'placeholder hint'
+bug3.id = 3
+bug3.fact = 'placeholder fact'
 bugs.bugs.push(bug3)
 
 const bug4 = {}
 bug4.name = 'Mimas tiliae'
+bug4.image = './public/images/moth4.jpeg'
+bug4.hint = 'placeholder hint'
+bug4.id = 4
+bug4.fact = 'placeholder fact'
 bugs.bugs.push(bug4)
 
 const bug5 = {}
-bug5.name = 'Green pergesa hawkmoth'
+bug5.name = 'Twin-spotted sphinx'
+bug5.image = './public/images/moth5.jpeg'
+bug5.hint = 'placeholder hint'
+bug5.id = 5
+bug5.fact = 'placeholder fact'
 bugs.bugs.push(bug5)
 
 const bug6 = {}
-bug6.name = 'Green pergesa hawkmoth'
+bug6.name = 'Daphnis nerii'
+bug6.image = './public/images/moth6.jpeg'
+bug6.hint = 'placeholder hint'
+bug6.id = 6
+bug6.fact = 'placeholder fact'
 bugs.bugs.push(bug6)
 
  console.log(JSON.stringify(bugs))
