308a2ebc21f3ccc9e40fdc951142ba402f5baae5
Mariano Zeppa <>
2021-01-14 23:03:50 +1300

    added new moths and images

--- createbugs.js
+++ createbugs.js
@@ -26,7 +26,7 @@ bug3.fact = 'The male has a wingspan of 20 cm (7.9 inches) and a tail span of 15
 bugs.bugs.push(bug3)
 
 const bug4 = {}
-bug4.name = 'lime hawk-moth'
+bug4.name = 'Lime hawk-moth'
 bug4.image = './public/images/moth4.jpeg'
 bug4.hint = 'It is found throughout the Palearctic region and the Near East, and has also been identified in eastern Canada'
 bug4.id = 4
@@ -34,7 +34,7 @@ bug4.fact = 'The wingspan is 70–80 millimetres (2.8–3.1 in)'
 bugs.bugs.push(bug4)
 
 const bug5 = {}
-bug5.name = 'Twin-spotted sphinx'
+bug5.name = 'Twin-spotted sphinx moth'
 bug5.image = './public/images/moth5.jpeg'
 bug5.hint = 'Adult moths are nocturnal, but seem to prefer the earlier part hours of the night'
 bug5.id = 5
@@ -42,11 +42,59 @@ bug5.fact = 'Smerinthus jamaicensis, the twin-spotted sphinx, is a moth of the f
 bugs.bugs.push(bug5)
 
 const bug6 = {}
-bug6.name = 'army green moth'
+bug6.name = 'Army green moth'
 bug6.image = './public/images/moth6.jpeg'
 bug6.hint = 'The caterpillars feed mainly on oleander (Nerium oleander) leaves, a highly toxic plant, to which the caterpillars are immune'
 bug6.id = 6
 bug6.fact = 'It is a migratory species, flying to parts of eastern and southern Europe during the summer, particularly Turkey'
 bugs.bugs.push(bug6)
+
+const bug7 = {}
+bug1.name = `Granny's cloak moth`
+bug1.image = './public/images/moth7.jpeg'
+bug1.hint = 'Caterpillars have long, flat bodies that have brown coats spotted with spots that are black or black and white.'
+bug1.id = 7
+bug1.fact = 'Australia native, typically found in the north eastern areas, but some have been randomly seen in New Zealand and Norfolk Island.'
+bugs.bugs.push(bug7)
+
+const bug8 = {}
+bug1.name = 'Rosy maple moth'
+bug1.image = './public/images/moth8.jpeg'
+bug1.hint = 'The emerging caterpillars mainly feed on the leaves of their host maple trees'
+bug1.id = 8
+bug1.fact = 'Popular choice in the pet trade.'
+bugs.bugs.push(bug8)
+
+const bug9 = {}
+bug1.name = 'Io moth'
+bug1.image = './public/images/moth9.jpeg'
+bug1.hint = ' Both male and female have one big black to bluish eyeo-spot with some white in the center'
+bug1.id = 9
+bug1.fact = 'After mating, the females lay eggs then wait to die.'
+bugs.bugs.push(bug9)
+
+const bug10 = {}
+bug1.name = 'Scarlet-bodied wasp moth'
+bug1.image = './public/images/moth10.jpeg'
+bug1.hint = 'Caterpillars are yellow-bodied and covered with a thick coat of white spiky fur.'
+bug1.id = 10
+bug1.fact = '"Your chance of surviving greatly increases if you look like something really dangerous,” said someone.'
+bugs.bugs.push(bug10)
+
+const bug11 = {}
+bug1.name = 'Tussar Silk Moth'
+bug1.image = './public/images/moth11.jpeg'
+bug1.hint = 'It is actually one of a number of tasar silkworms, species that produce silk'
+bug1.id = 11
+bug1.fact = 'This species is native to India.'
+bugs.bugs.push(bug11)
+
+const bug12 = {}
+bug1.name = `Death's-head hawkmoth`
+bug1.image = './public/images/moth12.jpeg'
+bug1.hint = 'It is a striking insect, but is seldom seen because it flies late in the night'
+bug1.id = 12
+bug1.fact = 'it is a powerful flier, having sometimes been found on ships far from land.'
+bugs.bugs.push(bug12)
 
  console.log(JSON.stringify(bugs))
