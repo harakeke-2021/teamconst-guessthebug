5fe0b2d549fd00662333fb2f122adff7764d2f2f
Peter <peter.lim91@gmail.com>
2021-01-15 01:09:04 +1300

    added comments

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const bugs = require('./bugs.json')
 const express = require('express')
 const hbs = require('express-handlebars')
+
+//function
+
+
+// './' get route
+// './' post route for handling the guess, and redirecting to correct/wrong answer page.
+// './:id' for just displaying the moths not required for MVP but should be easy enough to implement very quickly.
