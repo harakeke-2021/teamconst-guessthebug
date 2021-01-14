fa3bdbf47aae844e1e2869e2ea36e8ee7a0c0a8b
Peter <peter.lim91@gmail.com>
2021-01-15 12:07:16 +1300

    update correct.hbs

--- utils.js
+++ utils.js
@@ -1,16 +1,20 @@
 const bugs = require('./bugs.json')
 const { mountpath } = require('./server')
 
-function generateWrongAnswers() {
+function generateWrongAnswers(correctMothId) {
   const randomIds = []
   const numberOfBugs = 12
+
   while (randomIds.length < 3) {
-    let random = Math.floor(Math.random() * numberOfBugs)
-    if (!randomIds.includes(random)) {
-      randomIds.push(random)
+
+    const randomId = Math.floor(Math.random() * numberOfBugs) + 1
+    if (!(randomIds.includes(randomId) || randomId === correctMothId)) {
+      randomIds.push(randomId)
     }
   }
   const mothArray = bugs.moths.filter((moth) => randomIds.includes(moth.id))
+  console.log(randomIds)
   const wrongAnswers = mothArray.map((moth) => moth.name)
+  console.log(wrongAnswers)
   return wrongAnswers
 }
