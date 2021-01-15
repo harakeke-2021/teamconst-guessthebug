e89c22ec144444fbeefdd26e4e51eb18d4ac90d5
Mariano Zeppa <>
2021-01-15 13:15:07 +1300

    router.post

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const utils = require('./utils')
 const router = express.Router()
 const path = require('path')
 const server = require('./server')
+const { route } = require('./server')
 
 
 const home = path.join(__dirname, 'home')
@@ -14,6 +15,16 @@ router.use(express.urlencoded({
 
 router.get('/', (req, res) => {
   res.render('home')
+})
+
+router.post('/', (req, res) => {
+const theMoth = req.body
+console.log(theMoth)
+
+})
+
+router.post('/correct', (req, res) => {
+  res.render('')
 })
 
 router.post('/add', (req, res) => {
